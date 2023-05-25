287df8b331270e6961eaf6961aad7050d46a97fb
zachary-tarantinowoolson-sp <zachary.tarantino@sailpoint.com>
2023-05-25 19:44:26 -0400

    Removed service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-// This is the service worker script, which executes in its own context
-// when the extension is installed or refreshed (or when you access its console).
-
-console.log("service worker started")
-
-// not sure how servicer worker should work yet. This is just here so the extension will pass registration
-
-// will probably add Message Passing in here so console logs can go from the pop up to the main tab console
